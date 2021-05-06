afa12cd8010da8b9108adbf173a9a4e40b27d220
hzeroo <edusvh@gmail.com>
2021-05-06 17:09:28 +0200

    First commit

---
+++ src/main/java/com/jtheories/junit/JTheories.java
@@ -0,0 +1,10 @@
+package com.jtheories.junit;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.TestInstancePostProcessor;
+
+public class JTheories implements TestInstancePostProcessor {
+    public void postProcessTestInstance(Object o, ExtensionContext extensionContext) throws Exception {
+
+    }
+}
