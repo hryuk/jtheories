06576d24f74288d1e3a2d01de12f1270e089710b
Nirro <nireiorange@gmail.com>
2021-05-10 23:24:43 +0200

    Document and refactor GeneratorProcessor

--- examples/src/test/java/com/jtheories/examples/ProductGenerator.java
+++ examples/src/test/java/com/jtheories/examples/ProductGenerator.java
@@ -3,11 +3,14 @@ package com.jtheories.examples;
 import com.jtheories.core.generator.processor.Generator;
 import com.jtheories.generators.number.NotMultipleOf10;
 import com.jtheories.generators.number.Positive;
-
 import java.util.UUID;
 
 @Generator
 public interface ProductGenerator {
+
+  // TODO: Why force creating a default implementation to simply call new?
+  // Just call new by default and let the user provide a default implementation in case the created
+  // object needs any special syntax to be constructed (i.e. a builder)
   default Product generate(UUID id, String name, @Positive @NotMultipleOf10 Long price) {
     return new Product(id, name, price);
   }
--- examples/src/test/java/com/jtheories/examples/ProductTest.java
+++ examples/src/test/java/com/jtheories/examples/ProductTest.java
@@ -1,7 +1,7 @@
 package com.jtheories.examples;
 
-import java.util.UUID;
 import com.jtheories.junit.JTheoriesExtension;
+import java.util.UUID;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,6 +13,7 @@ class ProductTest {
   void doSomethingTest(Product product) {
     Assertions.assertEquals(product.getId(), UUID.fromString(product.getId().toString()));
     Assertions.assertTrue(product.getPrice() > 0);
+    Assertions.assertNotNull(product.getName());
     Assertions.assertNotEquals(product.getPrice() % 10, 0);
   }
 }
