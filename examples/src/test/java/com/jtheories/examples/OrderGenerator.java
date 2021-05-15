02003cd644b0129ce6bac5300f52bf37cb3ae0ee
hzeroo <edusvh@gmail.com>
2021-05-16 01:22:35 +0200

    Fixes OrderGenerator issue caused by Product not implementing Comparable

--- examples/src/test/java/com/jtheories/examples/OrderGenerator.java
+++ examples/src/test/java/com/jtheories/examples/OrderGenerator.java
@@ -2,11 +2,11 @@ package com.jtheories.examples;
 
 import com.jtheories.core.generator.processor.Generator;
 
-import java.util.List;
+import java.util.Collection;
 
 @Generator
 public interface OrderGenerator {
-  default Order generate(List<Product> items) {
+  default Order generate(final Collection<Product> items) {
     return new Order(items);
   }
 }
