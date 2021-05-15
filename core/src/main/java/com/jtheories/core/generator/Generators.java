e245e3d2ffc0c32411fcef5ef5ae1f1004f4957a
Nirro <nireiorange@gmail.com>
2021-05-15 22:21:45 +0200

    Godspeed
    Signed-off-by: Nirro <nireiorange@gmail.com>

--- core/src/main/java/com/jtheories/core/generator/Generators.java
+++ core/src/main/java/com/jtheories/core/generator/Generators.java
@@ -5,20 +5,24 @@ import com.jtheories.core.generator.exceptions.GeneratorInstantiationException;
 import io.github.classgraph.ClassGraph;
 import io.github.classgraph.ClassInfo;
 import io.github.classgraph.ClassInfoList;
-
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class Generators {
 
-  /** Expected name of the generator method */
+  /**
+   * Expected name of the generator method
+   */
   public static final String GENERATE = "generateConstrained";
+  private static final ConcurrentMap<Class<?>, Generator<?>> cache = new ConcurrentHashMap<>();
 
   private Generators() {
     throw new AssertionError("This class cannot be instanced");
   }
 
-  public static <T> Generator<T> getGenerator(Class<T> generatedType) {
+  private static <T> Generator<T> createGenerator(Class<T> generatedType) {
     try (var scanResult = new ClassGraph().enableClassInfo().scan()) {
       ClassInfoList arbitraryGenerators =
           scanResult.getClassesImplementing("com.jtheories.core.generator.Generator");
@@ -43,6 +47,11 @@ public class Generators {
       throw new GeneratorInstantiationException(
           String.format("Could not instantiate generator <%s>", e.getClass().getName()));
     }
+  }
+
+  public static <T> Generator<T> getGenerator(Class<T> generatedType) {
+    //noinspection unchecked
+    return (Generator<T>) cache.computeIfAbsent(generatedType, Generators::createGenerator);
   }
 
   public static <T> T gen(Class<T> generatedType, Class<?>... annotations) {
