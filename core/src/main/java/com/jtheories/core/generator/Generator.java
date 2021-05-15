a9c6deda3f59dbe431690d3e53080a109194a2db
Nirro <nireiorange@gmail.com>
2021-05-16 00:22:39 +0200

    Generate random collection subclasses Make generated lists mutable Add set generator
    Signed-off-by: Nirro <nireiorange@gmail.com>

--- core/src/main/java/com/jtheories/core/generator/Generator.java
+++ core/src/main/java/com/jtheories/core/generator/Generator.java
@@ -5,13 +5,14 @@ import java.lang.reflect.InvocationTargetException;
 public interface Generator<T> {
 
   default T generate() {
-    throw new RuntimeException();
+    throw new AssertionError("Wrong generate method invoked");
   }
 
   default T generate(Class<T> type) {
-    throw new RuntimeException();
+    throw new AssertionError("Wrong generate method invoked");
   }
 
+  // TODO: Remove exceptions from this method signature and throw a Runtime instead
   T generateConstrained(Class<?> type, Class<?>... annotations)
       throws NoSuchMethodException, InvocationTargetException, IllegalAccessException;
 }
